d9033ccadec0bd6fb0d547661e65597f027029b6
Anatolii <anatolii.radionov2002@gmail.com>
2024-05-22 19:30:40 +0200

    ImGui library
    In this commit ImGui library was added to the project.

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,9 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/vec4.hpp>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main() 
 {
@@ -34,8 +37,21 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    
+
     glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+
+    /* ImGui Init */
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.IniFilename = NULL;
+    io.LogFilename = NULL;
+    ImGui::StyleColorsLight();
+    ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow*>(window), true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+    float my_color[4] = {256.f, 256.f, 256.f, 256.f};
+    bool my_tool_active = true; 
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -43,12 +59,56 @@ int main()
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+
+
+        ImGui::NewFrame();
+
+        // Create a window called "My First Tool", with a menu bar.
+        ImGui::Begin("My First Tool", &my_tool_active, ImGuiWindowFlags_MenuBar);
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+                if (ImGui::MenuItem("Save", "Ctrl+S"))   { /* Do stuff */ }
+                if (ImGui::MenuItem("Close", "Ctrl+W"))  { my_tool_active = false; }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
+        }
+
+        // Edit a color stored as 4 floats
+        ImGui::ColorEdit4("Color", my_color);
+
+        // Generate samples and plot them
+        float samples[100];
+        for (int n = 0; n < 100; n++)
+            samples[n] = sinf(n * 0.2f + ImGui::GetTime() * 1.5f);
+        ImGui::PlotLines("Samples", samples, 100);
+
+        // Display contents in a scrolling region
+        ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
+        ImGui::BeginChild("Scrolling");
+        for (int n = 0; n < 50; n++)
+            ImGui::Text("%04d: Some text", n);
+        ImGui::EndChild();
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
