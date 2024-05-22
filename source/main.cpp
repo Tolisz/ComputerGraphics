95450d689ba0a3939ed3c9db8334913e5f4b4582
Anatolii <anatolii.radionov2002@gmail.com>
2024-05-22 18:37:24 +0200

    a Hello World window
    GLFW and glad libraries was added to the project. A simple example window is created on application start

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <glm/vec4.hpp>
 
 int main() 
@@ -7,5 +10,46 @@ int main()
     glm::vec4 v(1.0f);
 
     std::cout << v.x << " " << v.y << " " << v.z << " " << v.w << " " << std::endl;
+
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Initialize glad */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
